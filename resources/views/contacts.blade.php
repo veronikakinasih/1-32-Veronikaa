24e66fe08e8444d5bd9d0706d6fe2090bdca7c1c
veronikakinasih <3103120226@student.smktelkom-pwt.sch.id>
2022-06-07 14:15:39 +0700

    modul 4

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -3,17 +3,18 @@
 @section('container')
 <div class="title-top mb-5">
     <h2 class="fw-bold">Contact Us </h2> 
-<form>
+<form method="post" action="{{route('contacts.store')}}">
+  {{csrf_field()}}
 <div class="form-floating mb-3">
-  <input type="text" class="form-control" id="inputname" placeholder="Nama">
+  <input type="text" class="form-control" id="inputname" placeholder="Nama" name="nama">
   <label for="inputname">Nama</label>
 </div>
 <div class="form-floating mb-3">
-  <input type="email" class="form-control" id="inputemail" placeholder="Email">
+  <input type="email" class="form-control" id="inputemail" placeholder="Email" name="email">
   <label for="inputemail">Email</label>
 </div>
 <div class="form-floating mb-3">
-  <textarea class="form-control" placeholder="Tinggalkan pesan disini!" id="floatingTextarea2" style="height: 100px"></textarea>
+  <textarea class="form-control" placeholder="Tinggalkan pesan disini!" id="floatingTextarea2" style="height: 100px" name="pesan"></textarea>
   <label for="floatingTextarea2"> Pesan </label>
 </div>
   <button type="submit" class="btn btn-primary">Submit</button>
