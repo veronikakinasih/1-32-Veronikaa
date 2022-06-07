9a1b745b4f647bc3b0e2dca120dceedc66af45f2
veronikakinasih <3103120226@student.smktelkom-pwt.sch.id>
2022-06-07 14:50:16 +0700

    modul 5

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ Route::get('/', function () {
 });
 
 Route::get('/', function (){
-    return view('home', [
-        "title" => "Home"
+    return view('index', [
+        "title" => "Beranda"
     ]);
 });
 
@@ -45,3 +45,7 @@ Route::get('/contacts', function (){
     ]);
 });
 Route::resource('/contacts', ContactController::class);
+Auth::routes();
+
+Route::group(['middleware' => ['auth']], function () {Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
